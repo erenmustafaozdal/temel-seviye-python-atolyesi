21714dbab960212bbf92de36274ec05deb491b2f
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2021-12-28 22:43:45 +0300

    1. hafta uygulamaları

---
+++ uygulamalar_hafta1.py
@@ -0,0 +1,59 @@
+# UYGULAMALAR
+# Her uygulama örneği bir yorum parçasının altına yapılabilir
+# //////////////////////////////////////////////////////////////////////////////
+
+"""
+    1- Bir öğrenci için aşağıdaki bilgileri içeren değişkenler oluşturunuz
+    Öğrenci adı
+    Öğrenci soyadı
+    Öğrenci ad + soyad
+    Öğrenci cinsiyet
+    Öğrenci tc kimlik (?: sayı mı? metin mi?)
+    Öğrenci doğum yılı
+    Öğrenci yaşı
+"""
+# ogrenci_ad -> ogrenciAd
+ogrenci_ad = "Eren"
+ogrenci_soyad = "Özdal"
+ad_soyad = ogrenci_ad + " " + ogrenci_soyad
+print(ad_soyad)
+
+
+"""
+    2- Aşağıdaki yazılı notlarını yüzdelerine göre hesaplayıp, öğrencinin karne notunu bulunuz
+    1. yazılı: 75 (%40)
+    2. yazılı: 90 (%60)
+"""
+
+
+'''
+    3- Kullanıcıdan yarı çapı alınan bir dairenin alanını ve çevresini hesaplayınız
+    Daire Alanı   : πr²
+    Daire Çevresi : 2πr
+
+    * Yarı çap kullanıcıdan alınacak: r
+    * π: 3.14
+'''
+pi = 3.14
+
+
+url = "https://istanbulakademi.meb.gov.tr/akademiler.php?pID=759"
+atolye  = "Temel Seviye Python Atölyesi"
+
+# 4- 'url' içinde kaç karakter bulunmaktadır ?
+
+
+# 5- 'İstanbul öğretmen Akademileri' ifadesindeki ö harfini 'Ö' ile değiştirin.
+
+
+# 6- Karakter dizisi içinde belirli bir ifadeyi arayıp indeks numarasını alalım: find()
+
+# 7- Karakter dizisi içindeki karakterlerin hepsi alfabetik mi: isalpha()
+
+# 8- Karakter dizisi içindeki karakterlerin hepsi sayısal mı: isdigit()
+
+# 9- Karakter dizisi belirli bir ifade ile mi başlıyor: startswith()
+
+# 10- Karakter dizisi belirli bir ifade ile mi bitiyor: endswith()
+
+# 11- Karakter dizisinde belirli bir ifadeyi başka bir ifade ile yer değiştirelim: replace()
